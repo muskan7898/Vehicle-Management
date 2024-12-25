812d8ba7c40a4cc057fbd9e46056f818b5588c4b
muskan7898 <muskannareliya7@gmail.com>
2024-12-25 09:18:27 +0530

    Initial commit

---
+++ VehicalManagement/src/main/java/com/vehicleManagement/VehicalManagement/VehicalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.vehicleManagement.VehicalManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicalManagementApplication.class, args);
+	}
+
+}
---
+++ VehicalManagement/src/test/java/com/vehicleManagement/VehicalManagement/VehicalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehicleManagement.VehicalManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
